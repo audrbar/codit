b1c106b596b9f8e53dd705dec451b6453053e25d
audrbar <audrbar@gmail.com>
2023-10-31 19:47:39 +0200

    sign-modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
     children,
+    authModal,
 }: {
     children: React.ReactNode;
+    authModal: React.ReactNode;
 }) {
     return (
         <html
@@ -25,7 +27,9 @@ export default function RootLayout({
             )}
         >
             <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+                {/* @ts-expect-error server component */}
                 <Navbar />
+                {authModal}
                 <Toaster position="top-center" reverseOrder={false} />
                 <div className="container max-w-7xl mx-auto h-full pt-12">
                     {children}
