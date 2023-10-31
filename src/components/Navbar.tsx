58cc13a680088b6d71f76e99b228384e1db7aa72
audrbar <audrbar@gmail.com>
2023-10-31 19:28:11 +0200

    auth-working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
-// import { authOptions } from '@/lib/auth'
-// import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import { Icons } from './Icons';
 import { buttonVariants } from './ui/Button';
-// import { UserAccountNav } from './UserAccountNav'
+import { UserAccountNav } from './UserAccountNav';
 // import SearchBar from './SearchBar'
 
 const Navbar = async () => {
-    //   const session = await getServerSession(authOptions)
+    const session = await getServerSession(authOptions);
     return (
         <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
             <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -21,10 +21,13 @@ const Navbar = async () => {
 
                 {/* search bar */}
 
-                {/* actions */}
-                <Link href="/sign-in" className={buttonVariants()}>
-                    Sign In
-                </Link>
+                {session?.user ? (
+                    <UserAccountNav user={session.user} />
+                ) : (
+                    <Link href="/sign-in" className={buttonVariants()}>
+                        Sign In
+                    </Link>
+                )}
             </div>
         </div>
     );
