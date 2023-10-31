28eaefdc56b633974b0b2b27961d099292c9d6ae
audrbar <audrbar@gmail.com>
2023-10-31 17:42:09 +0200

    login_pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,34 @@
-import '@/styles/globals.css'
+import { cn } from '@/lib/utils';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/Navbar';
+import '@/styles/globals.css';
 
-export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'audrbar | Codit',
+    description: 'A Reddit clone built with Next.js and TypeScript.',
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html
+            lang="en"
+            className={cn(
+                'bg-white text-slate-900 antialiased light',
+                inter.className
+            )}
+        >
+            <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+                <Navbar />
+                <div className="container max-w-7xl mx-auto h-full pt-12">
+                    {children}
+                </div>
+            </body>
+        </html>
+    );
 }
