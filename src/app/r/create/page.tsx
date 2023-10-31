5fbbb9bdfa3772393dc5148a75f223ba4fe771a6
audrbar <audrbar@gmail.com>
2023-10-31 22:16:41 +0200

    create

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -2,8 +2,8 @@
 
 import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
-// import { toast } from '@/hooks/use-toast';
-// import { useCustomToasts } from '@/hooks/use-custom-toasts';
+import { toast } from '@/hooks/use-toast';
+import { useCustomToasts } from '@/hooks/use-custom-toasts';
 import { CreateSubredditPayload } from '@/lib/validators/subreddit';
 import { useMutation } from '@tanstack/react-query';
 import axios, { AxiosError } from 'axios';
@@ -13,7 +13,7 @@ import { useState } from 'react';
 const Page = () => {
     const router = useRouter();
     const [input, setInput] = useState<string>('');
-    // const { loginToast } = useCustomToasts();
+    const { loginToast } = useCustomToasts();
 
     const { mutate: createCommunity, isLoading } = useMutation({
         mutationFn: async () => {
