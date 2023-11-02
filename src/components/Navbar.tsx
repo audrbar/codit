c6d21c0d897ed94322e795dc686d52938353b26a
audrbar <audrbar@gmail.com>
2023-11-02 18:45:11 +0200

    search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,14 @@ import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import { Icons } from './Icons';
+import SearchBar from './SearchBar';
 import { buttonVariants } from './ui/Button';
 import { UserAccountNav } from './UserAccountNav';
-// import SearchBar from './SearchBar'
 
 const Navbar = async () => {
     const session = await getServerSession(authOptions);
     return (
-        <div className="fixed top-0 inset-x-0 h-[3.5rem] bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
             <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
                 {/* logo */}
                 <Link href="/" className="flex gap-2 items-center">
@@ -20,6 +20,7 @@ const Navbar = async () => {
                 </Link>
 
                 {/* search bar */}
+                <SearchBar />
 
                 {session?.user ? (
                     <UserAccountNav user={session.user} />
